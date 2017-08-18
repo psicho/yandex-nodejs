4f4f0ef6f98316ab7b6c91c0da5724a7530d5560
psicho <9400878@gmail.com>
2017-08-19 01:05:40 +0300

    Сделан рассчёт пробелов в строке ФИО. Нужно добавить анализ букв.

--- index.js
+++ index.js
@@ -1,44 +1,29 @@
-"use strict";
+'use strict';
 
 console.log('Тестовое задание Yandex');
 
-function submiting () {
-	var div = document.getElementById('resultContainer');
-    var form = document.getElementById('myForm')
-	var fio = form.elements.fio;
-	var email = form.elements.email;
-	var phone = form.elements.phone;
-//	alert(fio.value +'\n' + email.value + '\n' + phone.value);
-	div.innerText = fio.value + '\n' + email.value + '\n' + phone.value
+var div = document.getElementById('resultContainer');
+var form = document.getElementById('myForm');
+var fio = form.elements.fio;
+var email = form.elements.email;
+var phone = form.elements.phone;
 
-//	div.innerHTML = 'Test'
-//    alerting ();
-//	alert('div ' + div.value);
-    }
-
-function fio () {
-    var form = document.getElementById('myForm')
-    var div = document.getElementById('resultContainer');
-    var fio = form.elements.fio;
-    var p = 0;
-    div.innerText = 'p = ' + p;
-    alert(fio);
-    for (i = 0; i <= fio.length ; i++) {
-        if (fio[i] === ' ') {
-            p = p++;
-        }
-
-    }
-    div.innerText = p;
-    alert(p);
-
+function submiting() {
+	div.innerText = fio.value + '\n' + email.value + '\n' + phone.value;
 }
 
-function alerting () {
-    var div = document.getElementById('resultContainer');
-    div.innerText = 'ФИО ' + fio.value;
-//    document.body.appendChild(div);
+function fiof() {
+    var p = 0, i, fio1;
+    fio1 = fio.value;
+    console.log('fio1.length ' + fio1.length);
+    for (i = 0; i < fio1.length; i++) {
+        if (fio1[i] === ' ') {
+            p = p + 1;
+        }
     }
-var div = document.getElementById('resultContainer');
-div.innertext = fio.value + '\n' + email.value + '\n' + phone.value
-//console.log(fio.value +'\n' + email.value + '\n' + phone.value);
+    div.innerText = 'пробелов в ФИО ' + p;
+}
+
+function alerting() {
+    div.innerText = 'ФИО ' + fio.value;
+}
