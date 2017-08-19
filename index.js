6d47fc70ab38516b1084a6195c42f2383ab1ecc5
psicho <9400878@gmail.com>
2017-08-19 14:13:47 +0300

    Доработан анализ ФИО.

--- index.js
+++ index.js
@@ -7,21 +7,37 @@ var form = document.getElementById('myForm');
 var fio = form.elements.fio;
 var email = form.elements.email;
 var phone = form.elements.phone;
+var err = 0;
 
 function submiting() {
 	div.innerText = fio.value + '\n' + email.value + '\n' + phone.value;
 }
 
 function fiof() {
-    var p = 0, i, fio1;
+    var p = 0, w = 0, slo = 0, i, fio1;
     fio1 = fio.value;
     console.log('fio1.length ' + fio1.length);
     for (i = 0; i < fio1.length; i++) {
-        if (fio1[i] === ' ') {
+//        console.log('fio1[i].match= ' + fio1[i].match(/[a-zA-Zа-яА-Я]/));
+        if (fio1[i].match(/[a-zA-Zа-яА-Я ]/) !== null) {
+            w = w + 1;
+            if (fio1[i] === ' ' & w > 0) {
             p = p + 1;
+            console.log('w = ' + w)
+            w = 0;
+            slo = slo + 1;
+            }
+            if (p == 2 & i == fio1.length - 1 & w > 0) {
+            slo = slo + 1;
+            }
+        } else {
+            err = 1;
+            return div.innerText = 'Ошибка!!!';
+//            break;
         }
+
     }
-    div.innerText = 'пробелов в ФИО ' + p;
+    div.innerText = 'пробелов = ' + p + '; слов = ' + slo;
 }
 
 function alerting() {
