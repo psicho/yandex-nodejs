1112a88a73dc06f3501d71a6d98cda34911b7e23
psicho <9400878@gmail.com>
2017-08-28 14:30:54 +0400

    Доработаны все мотоды объекта MyForm

--- index.js
+++ index.js
@@ -114,64 +114,61 @@ function emailf() {
     }
 }
 
-$(document).ready(function () {
-    div = document.getElementById('resultContainer');
-    div.innerText = "Progress";
-    div.setAttribute('class', 'progress');
-    $('.progress').hide();
-    $('#submitButton').on('click', function () {
-		var errString = "";
-        setTimeout(function() {progr()},0);
-//        console.log('OK');
-        fiof();
-        emailf();
-        phonef();
-        div.removeAttribute('style');
-        $('.progress').show();
-        if (errphone === 0 && erremail === 0 && errfio === 0) {
-            email.removeAttribute('class');
-            phone.removeAttribute('class');
-            fio.removeAttribute('class');
-            $('#submitButton').hide();
-            $('.progress').show();
-
-
-            var i = Math.round(Math.random()) % 2;
-            if (i === 0) {
-                div.setAttribute('class', 'success');
-                div.innerText = "Success";
-                $.getJSON('success.json', {}, function (json) {
-                    div.setAttribute('class', 'success');
-                    div.innerText = "Success";
-                    console.log(JSON.stringify(json));
-                    });
-                return;
-            }
-            else if (i === 1){
-                div.setAttribute('class', 'progress');
-                div.innerText = "Progress";
-				contin = 1;
-                $.getJSON('progress.json', {}, function (json) {
-                        console.log(JSON.stringify(json));
-//                        sleeping(3)
-                    });
-            }
-
-
-        }
-        if (errphone === 1 || erremail === 1 || errfio === 1) {
-//			var errString = "";
-            $.getJSON('error.json', {}, function (json) {
+//$(document).ready(function () {
+//    div = document.getElementById('resultContainer');
+//    div.innerText = "Progress";
+//    div.setAttribute('class', 'progress');
+//    $('.progress').hide();
+//    $('#submitButton').on('click', function () {
+//		var errString = "";
+//        setTimeout(function() {progr()},0);
+//        fiof();
+//        emailf();
+//        phonef();
+//        div.removeAttribute('style');
+//        $('.progress').show();
+//        if (errphone === 0 && erremail === 0 && errfio === 0) {
+//            email.removeAttribute('class');
+//            phone.removeAttribute('class');
+//            fio.removeAttribute('class');
+//            $('#submitButton').hide();
+//            $('.progress').show();
+//
+//
+//            var i = Math.round(Math.random()) % 2;
+//            if (i === 0) {
+//                div.setAttribute('class', 'success');
+//                div.innerText = "Success";
+//                $.getJSON('success.json', {}, function (json) {
+//                    div.setAttribute('class', 'success');
+//                    div.innerText = "Success";
+//                    console.log(JSON.stringify(json));
+//                    });
+//                return;
+//            }
+//            else if (i === 1){
+//                div.setAttribute('class', 'progress');
+//                div.innerText = "Progress";
+//				contin = 1;
+//                $.getJSON('progress.json', {}, function (json) {
+//                        console.log(JSON.stringify(json));
+//                    });
+//            }
+//
+//
+//        }
+//        if (errphone === 1 || erremail === 1 || errfio === 1) {
+//            $.getJSON('error.json', {}, function (json) {
 //                        console.log('err ' + JSON.stringify(json));
-                        div.setAttribute('class', 'error');
-						if (errfio === 1) {errString = errString + fio.value + "\n";}
-						if (erremail === 1) {errString = errString + email.value + "\n";}
-                        if (errphone === 1) {errString = errString+ phone.value + "\n";}
-                        div.innerText = errString;
-                    });
-        }
-    })
-})
+//                        div.setAttribute('class', 'error');
+//						if (errfio === 1) {errString = errString + fio.value + "\n";}
+//						if (erremail === 1) {errString = errString + email.value + "\n";}
+//                        if (errphone === 1) {errString = errString+ phone.value + "\n";}
+//                        div.innerText = errString;
+//                    });
+//        }
+//    })
+//})
 
 function sleeping(seconds) {
 	var date;
@@ -237,12 +234,109 @@ MyForm.validate = function () {
     if (errphone === 0 && erremail === 0 && errfio === 0) {
         isValid = true;
     } else isValid = false;
-    if (errfio === 1) {errorFields.push('ФИО');}
-    if (erremail === 1) {errorFields.push('Почта');}
-    if (errphone === 1) {errorFields.push('Телефон');}
-    console.log(errorFields);
-    var val = {'isValid': isValid, 'errorField': errorFields}
-    return val;
+    if (errfio === 1) {errorFields.push('fio');}
+    if (erremail === 1) {errorFields.push('email');}
+    if (errphone === 1) {errorFields.push('phone');}
+//    console.log(errorFields);
+    var valobj = {'isValid': isValid, 'errorField': errorFields};
+    var valtext = '{isValid: '+isValid+', errorField: '+errorFields+'}';
+    return valtext;
 }
 
+MyForm.getData = function () {
+    this.fio = fio.value;
+    this.email = email.value;
+    this.phone = phone.value;
+    var dataf = {'fio':fio.value, 'email':email.value, 'phone':phone.value};
+    return dataf;
+}
+
+MyForm.setData = function (Object) {
+    var fios = Object.fio;
+    var emails = Object.email;
+    var phones = Object.phone;
+    fio.value = fios;
+    email.value = emails;
+    phone.value = phones;
+    fiof();
+    emailf();
+    phonef();
+}
+
+MyForm.submit = function () {
+    div = document.getElementById('resultContainer');
+    div.innerText = "Progress";
+    div.setAttribute('class', 'progress');
+    $('.progress').hide();
+    var errString = "";
+    setTimeout(function() {progr()},0);
+    fiof();
+    emailf();
+    phonef();
+    div.removeAttribute('style');
+    $('.progress').show();
+    if (errphone === 0 && erremail === 0 && errfio === 0) {
+        email.removeAttribute('class');
+        phone.removeAttribute('class');
+        fio.removeAttribute('class');
+        $('#submitButton').hide();
+        $('.progress').show();
+
+        var i = Math.round(Math.random()) % 2;
+        if (i === 0) {
+            div.setAttribute('class', 'success');
+            div.innerText = "Success";
+            sleeping(1);
+            $.getJSON('success.json', {}, function (json) {
+                div.setAttribute('class', 'success');
+                div.innerText = "Success";
+                console.log(JSON.stringify(json));
+                });
+            return;
+        }
+        else if (i === 1){
+            div.setAttribute('class', 'progress');
+            div.innerText = "Progress";
+            contin = 1;
+            $.getJSON('progress.json', {}, function (json) {
+                    console.log(JSON.stringify(json));
+//                        sleeping(3)
+                });
+        }
+
+
+    }
+    if (errphone === 1 || erremail === 1 || errfio === 1) {
+//			var errString = "";
+        $.getJSON('error.json', {}, function (json) {
+//                        console.log('err ' + JSON.stringify(json));
+                    div.setAttribute('class', 'error');
+                    if (errfio === 1) {errString = errString + fio.value + "\n";}
+                    if (erremail === 1) {errString = errString + email.value + "\n";}
+                    if (errphone === 1) {errString = errString+ phone.value + "\n";}
+                    div.innerText = errString;
+                });
+    }
+}
+
+console.log('\n')
+console.log('Работа объекта MyForm')
+console.log('Первоначально форме передаются следующие данные:')
+console.log('fio: Иванов Иван Иванович, email: 12345@ya.ru, phone: +7(111)111-15-56')
+
+console.log('\n')
+console.log('Работа метода MyForm.validate()')
 console.log(MyForm.validate())
+
+console.log('\n')
+console.log('Работа метода MyForm.getData()')
+console.log(MyForm.getData())
+
+console.log('\n')
+console.log('Работа метода MyForm.setData()')
+var NewOb = {'fio':'Миронов Андрей Викторович', 'email': 'mironov@yandex.ru', 'phone': '+7(111)222-21-32'}
+MyForm.setData(NewOb)
+console.log('Передаём новый объект: {"fio":"Миронов Андрей Викторович", "email": "mironov@yandex.ru", "phone": "+7(111)222-21-32"')
+console.log('Результат работы метода MyForm.setData()')
+console.log(MyForm.getData())
+console.log('Также эти данные отопражаются также и в текущем представлении формы.')
